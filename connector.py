0509b9059a580094cf3f99d3489912d511372d28
Ehong-Sun <esun7@jhu.edu>
2022-11-22 11:05:06 -0500

    Forgot to push my code

--- connector.py
+++ connector.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
 import logging
-import os
 import sys
 import json
 import jsonschema
+import os
 import model
 
-
 logging.basicConfig(level=logging.DEBUG)
+
+model_input_fn = sys.argv[1]
+model_output_fn = sys.argv[2]
+model_input_schema_fn = sys.argv[3]
+model_output_schema_fn = sys.argv[4]
 
 model_description = {
     "name": "model-connector-tutorial",
@@ -15,12 +19,8 @@ model_description = {
     "connectorVersion": os.getenv("CONNECTOR_VERSION"),
 }
 
-model_input_fn = sys.argv[1]
-model_output_fn = sys.argv[2]
-model_input_schema_fn = sys.argv[3]
-model_output_schema_fn = sys.argv[4]
-
 logging.info('Starting connector')
+
 # Read input and validate
 with open(model_input_fn) as f:
     model_input = json.load(f)
@@ -30,24 +30,10 @@ with open(model_input_schema_fn) as f:
     model_input_schema = json.load(f)
     jsonschema.validate(model_input, model_input_schema)
 
-model_output = {
-    'outputs': [],
-    't': [],
-    'u': [],
-}
-
-logging.info('Executing simulation')
 model_output = model.simulate(**model_input)
 model_output['metadata'] = model_input
 model_output['model'] = model_description
-logging.debug(f'Simulation result: {model_output}')
-
-# Save outputs
-with open(model_output_fn, 'w') as f:
-    json.dump(model_output, f, indent='  ')
-
-logging.info('Simulation successfully completed')
-logging.debug(f'Simulation result: {model_output}')
+logging.debug(f'Simulation results: {model_output}')
 
 with open(model_output_schema_fn) as f:
     model_output_schema = json.load(f)
@@ -57,4 +43,4 @@ with open(model_output_schema_fn) as f:
 with open(model_output_fn, 'w') as f:
     json.dump(model_output, f, indent='  ')
 
-logging.info(f'Simulation successfully completed')
+logging.info('Simulation successfully completed')
